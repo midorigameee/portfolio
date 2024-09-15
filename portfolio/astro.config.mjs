c320b434638a4301f47064ae0036866bc1237cd6
midorigameee <xqbodp727@gmail.com>
2024-09-15 07:09:07 +0000

    Astroのデプロイ設定を変更

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig(
   {
     integrations: [tailwind()],
     site: 'https://midorigameee.github.io',
-    base: '/portfolio',
+    // base: '/portfolio',
   }, // your configuration options here...
   // https://docs.astro.build/en/reference/configuration-reference/
 );
