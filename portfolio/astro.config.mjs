3d81daa6b7387440accc9759b0b7e03817a6ffe0
midorigameee <xqbodp727@gmail.com>
2024-09-15 07:42:43 +0000

    変更を一回全部戻す

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -8,11 +8,11 @@ export default defineConfig(
     integrations: [tailwind()],
     site: 'https://midorigameee.github.io',
     base: '/portfolio_astro',
-    root: './portfolio',
-    publicDir: './portfolio/public',
-    srcDir: './portfolio/src',
-    outDir: './portfolio/dist',
-    cacheDir: './portfolio/node_modules/.astro',
+    // root: './portfolio',
+    // publicDir: './portfolio/public',
+    // srcDir: './portfolio/src',
+    // outDir: './portfolio/dist',
+    // cacheDir: './portfolio/node_modules/.astro',
   }, // your configuration options here...
   // https://docs.astro.build/en/reference/configuration-reference/
 );
