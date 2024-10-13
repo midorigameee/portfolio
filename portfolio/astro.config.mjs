6c9bd832684c15e25f821e916ae824c4ba6aab7a
midorigameee <xqbodp727@gmail.com>
2024-10-13 05:29:09 +0000

    GitHub Pagesビルドの設定変更

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig(
   {
     integrations: [tailwind()],
-    site: 'https://midorigameee.github.io/',
+    site: 'https://midorigameee.github.io',
     base: '/portfolio',
     // trailingSlash: 'never',
     // build: {
