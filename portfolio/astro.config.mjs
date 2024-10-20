ed552e1faa43ab9cd5c43e561ecc08c939823b47
midorigameee <xqbodp727@gmail.com>
2024-10-20 12:55:22 +0000

    開発環境でサーバーが立ち上がらない問題を解決

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+// 本番環境のみtrue
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://midorigameee.github.io',
-  base: '/portfolio_astro/',
+  base: isProduction ? '/portfolio_astro/' : '',
   trailingSlash: 'never',
 });
